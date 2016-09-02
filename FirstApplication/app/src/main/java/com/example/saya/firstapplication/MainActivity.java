852fcf1bb997b431fa06a05ac9c01b0fa435448d
sayaten <ilyfstory@gmail.com>
2016-09-02 18:36:08 +0900

    intent test

--- FirstApplication/app/src/main/java/com/example/saya/firstapplication/MainActivity.java
+++ FirstApplication/app/src/main/java/com/example/saya/firstapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.saya.firstapplication;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButton1Click(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
 }
