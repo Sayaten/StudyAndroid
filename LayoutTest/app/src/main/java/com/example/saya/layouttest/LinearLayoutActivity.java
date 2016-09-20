ac99165318d21423a422c3f415901ed2ba4dbbce
sayaten <ilyfstory@gmail.com>
2016-09-20 20:44:03 +0900

    레이아웃 공부 1

---
+++ LayoutTest/app/src/main/java/com/example/saya/layouttest/LinearLayoutActivity.java
@@ -0,0 +1,17 @@
+package com.example.saya.layouttest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LinearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+    }
+}
---
+++ LayoutTest/app/src/main/java/com/example/saya/layouttest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.saya.layouttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.padding);
+    }
+}
